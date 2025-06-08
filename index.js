d7e3719753f8b26f1c0214d6c5ec6840f6a43228
Yasodha456 <yasodhamercy27@gmail.com>
2025-06-08 20:11:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ connectDB();
 app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(Server running on port ${PORT})); // fixed backticks
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
